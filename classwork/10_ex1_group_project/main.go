d50e8474ec9bc1229845e2ee0abad32f7f8ef99b
NightPlanet <pavlygin@inbox.ru>
2026-09-17 19:24:20 +0300

    Added project structure

--- classwork/10_ex1_group_project/main.go
+++ classwork/10_ex1_group_project/main.go
@@ -1,5 +1,45 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
+type Book struct {
+	ID     string
+	Author string
+	Title  string
+}
+
+func PrintState(avail []Book, inUse []Book) {
+
+}
+
+func PrintHelp() {
+
+}
+
+func FindBook(books []Book, s string) {
+
+}
+
+func main() {
+	avail := []Book{}
+	var inUse []Book
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		// Start new frame - clear Terminal buffer
+		fmt.Print("\033[H\033[2J")
+
+		// Print current state and help
+		PrintState(avail, inUse)
+		PrintHelp()
+
+		if scanner.Scan() {
+			text := scanner.Text()
+			// Parse command
+		}
+	}
 }
